d7117844fcf96bd9f0c2490596e536c21f02f849
Sarah Vessels <cheshire137@gmail.com>
2017-04-22 11:20:41 -0500

    Hit Esc to close modal, focus input on modal open

--- public/main.js
+++ public/main.js
@@ -1,7 +1,14 @@
 function openModal(event) {
+  event.preventDefault()
+
   const modalSelector = event.currentTarget.getAttribute('href')
   const modal = document.querySelector(modalSelector)
   modal.classList.add('is-active')
+
+  const focusTarget = modal.querySelector('.js-modal-focus')
+  if (focusTarget) {
+    focusTarget.focus()
+  }
 }
 
 function closeModal(event) {
@@ -15,10 +22,24 @@ function setUpModals() {
     openLinks[i].addEventListener('click', openModal)
   }
 
-  const closeLinks = document.querySelectorAll('.modal-close')
+  const closeLinks = document.querySelectorAll('.js-modal-close')
   for (let i = 0; i < closeLinks.length; i++) {
     closeLinks[i].addEventListener('click', closeModal)
   }
 }
 
+function onPlaylistNameKeyup(event) {
+  if (event.keyCode === 27) { // Esc
+    closeModal(event)
+  }
+}
+
+function listenForEscape() {
+  const input = document.querySelector('.js-playlist-name-input')
+  if (input) {
+    input.addEventListener('keyup', onPlaylistNameKeyup)
+  }
+}
+
 setUpModals()
+listenForEscape()
