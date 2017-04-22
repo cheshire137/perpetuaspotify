793679b9ae621c83d583db2c3203384f491c82f3
Sarah Vessels <cheshire137@gmail.com>
2017-04-22 12:32:31 -0500

    Get track URI in model

--- models/spotify_track.rb
+++ models/spotify_track.rb
@@ -2,12 +2,13 @@ require_relative 'spotify_artist'
 
 class SpotifyTrack
   attr_reader :name, :artists, :url, :played_at, :id, :image_url,
-    :image_width, :image_height
+    :image_width, :image_height, :uri
   attr_accessor :audio_features
 
   def initialize(hash)
     @id = hash['id']
     @name = hash['name']
+    @uri = hash['uri']
     @artists = hash['artists'].map { |artist| SpotifyArtist.new(artist) }
     @url = hash['external_urls']['spotify']
     @played_at = if date_str = hash['played_at']
